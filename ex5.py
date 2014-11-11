6bbc6675d36a52771e8d6a42d306fafe7734216a
Jiren Jin <jinjiren@foxmail.com>
2014-11-11 20:54:05 +0900

    finished the first half

--- ex5.py
+++ ex5.py
@@ -17,5 +17,5 @@ print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." % ( \
+print "If I add %d, %d, and %d I get %d." % (
         age, height, weight, age + height + weight)
